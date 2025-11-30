fbe04e6e5c07e63f2142e0d4d76656b63b34f079
FK-0802 <fk.0802ac@gmail.com>
2025-11-30 19:08:42 +0900

    がんばった、重力判定に難あり

--- Assets/Script/Player/PlayerMove.cs
+++ Assets/Script/Player/PlayerMove.cs
@@ -3,12 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class PlayerMove : MonoBehaviour
 {
+    //初期設定
     private Vector2 startPosition;
     
-    public float runSpeed = 1f;
-    public float jumpForce = 3f;
+    //移動パラメータ
+    public float runSpeed;
 
+    public float jumpPower;
+    private bool jumpReqest;
+    
+    public float gravity;
+    private float verticalVelocity;
+
+    //物理演算
     private Rigidbody2D rb;
+
+    //トリガー
     private bool isGrounded;
     public bool isDeat = false; //死亡判定
 
@@ -17,31 +27,48 @@ public class PlayerMove : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
+        rb.linearVelocity = Vector2.zero;
         startPosition = transform.position;
     }
 
     private void FixedUpdate()
     {
-        //常に右に進む
-        rb.linearVelocity = new Vector2(runSpeed, rb.linearVelocity.y);
+        if (jumpReqest)
+        {
+            verticalVelocity = jumpPower;
+            jumpReqest = false;
+        }
+
+        //重力処理
+        verticalVelocity += gravity * Time.deltaTime;
+
+        //y方向の移動
+        transform.position += new Vector3(0, verticalVelocity * Time.deltaTime, 0);
+
+        print(verticalVelocity);
     }
 
     void Update()
     {
-        //ジャンプ（Space for 左クリック）
+        //常に右に進む
+        transform.Translate(Vector2.right * runSpeed * Time.deltaTime);
+
+        //ジャンプ（Spaceキー）
         if ((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) && isGrounded)
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            jumpReqest = true;
         }
         print(isGrounded);
-
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        print(collision.collider.name);
         if (collision.collider.CompareTag("Ground"))
         {
             isGrounded = true;
+            verticalVelocity = 0f;//着地時に落下速度をリセット
         }
         else if (collision.collider.CompareTag("Obstacle"))
         {
@@ -69,6 +96,7 @@ public class PlayerMove : MonoBehaviour
 
     private void Dedrestart()
     {
+        verticalVelocity = 0f;//着地時に落下速度をリセット
         rb.linearVelocity = Vector2.zero;
         transform.position = startPosition;
     }
